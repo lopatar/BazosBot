d460706ac4b40aaca7b9a5f06f6a064d066d1ffc
Jiří Lopatář <github@lopatar.me>
2022-12-18 10:21:11 +0100

    Fix

---
+++ BazosSpam/LocationProviders/CzLocationProvider.cs
@@ -0,0 +1,43 @@
+using BazosBotv2.Interfaces;
+
+namespace BazosBotv2.Bazos.LocationProviders;
+
+internal sealed class CzLocationProvider : ILocationProvider
+{
+    private KeyValuePair<string, string> _inputValidationField;
+
+    public string GetMyListingPath()
+    {
+        return "moje-inzeraty.php";
+    }
+
+    public string GetAddListingPath()
+    {
+        return "pridat-inzerat.php";
+    }
+
+    public string GetDeleteListingField()
+    {
+        return "Vymazat";
+    }
+
+    public KeyValuePair<string, string> GetInputValidationField()
+    {
+        return _inputValidationField;
+    }
+
+    public void SetInputValidationField(KeyValuePair<string, string> field)
+    {
+        _inputValidationField = field;
+    }
+
+    public Uri GetUri()
+    {
+        return new("https://bazos.cz/");
+    }
+    
+    public int GetZipCodeLength()
+    {
+        return 6;
+    }
+}
